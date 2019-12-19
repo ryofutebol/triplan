3f780f6f4ac7bfd245599005f56614d0877d4a87
Ryu <ryo.futebol@gmail.com>
2019-12-20 00:13:03 +0900

    インテンド修正

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,6 +17,8 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        $redirectTo = "/home";
+
         if ($guard === "admin") {
             $redirectTo = "/admin/home";
         }
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,12 @@ Route::get('/detail/{id}', 'ItemController@detail')->name('item.detail');
 
 Route::group(['prefix' => 'admin'], function() {
 	Route::get('/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-	Route::post('login',    'Admin\LoginController@login');
+	Route::post('login', 'Admin\LoginController@login');
 });
+
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
 	Route::get('/home', 'Admin\HomeController@index')->name('admin.home');
-	Route::post('logout',   'Admin\LoginController@logout')->name('admin.logout');
+	Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
 });
 Auth::routes();
 
