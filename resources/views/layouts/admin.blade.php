0805844118439c11aefbc7d715216ec00fdbd46b
奥野龍 <okunoryo@MBAR.local>
2020-03-07 17:28:25 +0900

    DB情報入力

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <!-- <style>body{background-color: skyblue;}</style> -->
 </head>
 <body>
