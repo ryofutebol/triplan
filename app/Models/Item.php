b9cc093cbc9d1c1891fc77ec37059bba10039404
奥野龍 <okunoryo@MBAR.local>
2020-03-08 23:02:04 +0900

    stockの記述

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
 	//データ挿入を許可するカラムを指定
-	protected $fillable = ['plan_name', 'prefecture', 'planner', 'comment', 'residence_history', 'price'];
+	protected $fillable = ['plan_name', 'prefecture', 'planner', 'comment', 'residence_history', 'stock', 'price'];
 
 	//使用するテーブルを指定
 	protected $table = 'items';
---
+++ database/migrations/2020_03_08_222504_delete_column_cart_id_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteColumnCartIdItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            $table->dropColumn('cart_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('items', function (Blueprint $table) {
+            //
+        });
+    }
+}
