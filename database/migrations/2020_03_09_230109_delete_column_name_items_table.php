15e1d7048b315f26b1a7d21a7b762eb1322ab40e
奥野龍 <okunoryo@MBAR.local>
2020-03-09 23:02:24 +0900

    items table name column delete

---
+++ database/migrations/2020_03_09_230109_delete_column_name_items_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DeleteColumnNameItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('name', function (Blueprint $table) {
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('name', function (Blueprint $table) {
+            //
+        });
+    }
+}
