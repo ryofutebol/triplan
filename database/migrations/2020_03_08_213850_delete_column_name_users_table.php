5df4ab3750e13b484f3adfb4f0f834886af7f71a
奥野龍 <okunoryo@MBAR.local>
2020-03-09 22:22:24 +0900

    users name rollback 削除

--- database/migrations/2020_03_08_213850_delete_column_name_users_table.php
+++ database/migrations/2020_03_08_213850_delete_column_name_users_table.php
@@ -26,7 +26,6 @@ class DeleteColumnNameUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
         });
     }
 }
