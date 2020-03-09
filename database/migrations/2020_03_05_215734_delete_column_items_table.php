f2183c0efc1b25571f411559554640e662f86fb3
奥野龍 <okunoryo@MBAR.local>
2020-03-09 22:31:04 +0900

    dropcolumn コメントアウト

--- database/migrations/2020_03_05_215734_delete_column_items_table.php
+++ database/migrations/2020_03_05_215734_delete_column_items_table.php
@@ -14,7 +14,7 @@ class DeleteColumnItemsTable extends Migration
     public function up()
     {
         Schema::table('items', function (Blueprint $table) {
-            $table->dropColumn('name');
+            // $table->dropColumn('name');
             $table->dropColumn('description');
             $table->dropColumn('stock');
             $table->dropColumn('price');
