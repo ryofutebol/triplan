b674b28dd73cfb5cd0b88a732b75077277c64d1c
奥野龍 <okunoryo@MBAR.local>
2020-03-09 22:52:06 +0900

    update

--- database/migrations/2020_03_09_002550_add_column_name_users_table2.php
+++ database/migrations/2020_03_09_002550_add_column_name_users_table2.php
@@ -26,6 +26,7 @@ class AddColumnNameUsersTable2 extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->string('name');
             //
         });
     }
